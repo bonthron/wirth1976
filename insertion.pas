b03d9d17163275384d7df956a42445e80d150246
jason <jason@pop-os.localdomain>
2020-10-02 14:41:40 -0400

    change procedures to functions, added shuffle

--- insertion.pas
+++ insertion.pas
@@ -14,52 +14,80 @@ type
   end;
 
 type
+  itemArray = array of item;
+
+type
   index = integer;
 
 
-var
-  r1: item = (key: 1; Value: 'A');
-var
-  r2: item = (key: 2; Value: 'B');
-var
-  r3: item = (key: 3; Value: 'C');
-var
-  r4: item = (key: 4; Value: 'D');
+{* ------------------------------------------------------------------ shuffle
+   The Fisher-Yates shuffle, in its original form, was described in 1938
+   by Ronald Fisher and Frank Yates in their book Statistical tables for
+   biological, agricultural and medical research.
+   The modern version of the Fisher-Yates shuffle, designed for computer use,
+   was introduced by Richard Durstenfeld in 1964 and popularized by
+   Donald E. Knuth in The Art of Computer Programming.
+   O(n)
+*}
+  function shuffle(arr: itemArray): itemArray;
+  var
+    i, j: index;
+    r: integer;
+    a: item;
+    b: item;
 
-var
-  a: array [0..3] of item;
+  begin
+    for i := (length(arr) - 1) downto 1 do
+    begin
+      r := random(i);
+      a := arr[i];
+      b := arr[r];
+      arr[i] := b;
+      arr[r] := a;
+    end;
 
-  procedure straightinsertion;
+    Result := arr;
+  end;
+
+
+  { -------------------------------------------------------- straightInsertion }
+  function straightinsertion(a: itemArray): itemArray;
   var
     i, j: index;
   var
     x: item;
 
   begin
-    for i := 1 to 3 do
+    for i := 1 to (length(a) - 1) do
     begin
       x := a[i];
       j := i - 1;
-      while ((j >= 0) and (a[j].key > x.key)) do
+      while ((j >= 0) and (a[j].key >= x.key)) do
       begin
         a[j + 1] := a[j];
         j := j - 1;
       end;
       a[j + 1] := x;
     end;
+    Result := a;
   end;
 
 
+var
+  a: array[0..9] of item = ((key: 0; Value: 'A'), (key: 1; Value: 'B'), (key: 2;
+    Value: 'C'), (key: 3; Value: 'D'), (key: 4; Value: 'E'), (key: 5;
+    Value: 'F'), (key: 6; Value: 'G'), (key: 7; Value: 'H'), (key: 8;
+    Value: 'I'), (key: 9; Value: 'J'));
+
+  b: itemArray;
+
+
 begin
-  a[0] := r4;
-  a[1] := r3;
-  a[2] := r2;
-  a[3] := r1;
 
-  Writeln(ToStr(a, TypeInfo(a)));
+  b := shuffle(a);
+  Writeln(ToStr(b, TypeInfo(b)));
 
-  straightinsertion();
-
-  Writeln(ToStr(a, TypeInfo(a)));
+  b := straightinsertion(b);
+  Writeln(ToStr(b, TypeInfo(b)));
 
 end.
